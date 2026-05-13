003b57324cd80f1206096f3421bd35e2a73c96fe
liaoweixiang2024-blip <106907398+liaoweixiang2024-blip@users.noreply.github.com>
2026-05-13 19:34:15 +0800

    fix: make cleanup migration idempotent

--- server/prisma/migrations/20260513130000_cleanup_orphaned_bom_tables/migration.sql
+++ server/prisma/migrations/20260513130000_cleanup_orphaned_bom_tables/migration.sql
@@ -2,10 +2,10 @@
 -- but still exist in the database from previously applied (and since deleted) migrations.
 
 -- DropForeignKey
-ALTER TABLE "selection_bom_media" DROP CONSTRAINT IF EXISTS "selection_bom_media_template_id_fkey";
-ALTER TABLE "selection_bom_rules" DROP CONSTRAINT IF EXISTS "selection_bom_rules_template_id_fkey";
-ALTER TABLE "selection_categories" DROP CONSTRAINT IF EXISTS "selection_categories_parent_id_fkey";
-ALTER TABLE "selection_templates" DROP CONSTRAINT IF EXISTS "selection_templates_category_id_fkey";
+ALTER TABLE IF EXISTS "selection_bom_media" DROP CONSTRAINT IF EXISTS "selection_bom_media_template_id_fkey";
+ALTER TABLE IF EXISTS "selection_bom_rules" DROP CONSTRAINT IF EXISTS "selection_bom_rules_template_id_fkey";
+ALTER TABLE IF EXISTS "selection_categories" DROP CONSTRAINT IF EXISTS "selection_categories_parent_id_fkey";
+ALTER TABLE IF EXISTS "selection_templates" DROP CONSTRAINT IF EXISTS "selection_templates_category_id_fkey";
 
 -- DropIndex
 DROP INDEX IF EXISTS "selection_categories_parent_id_idx";
